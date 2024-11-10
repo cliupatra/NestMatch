7e19b5693887c09fb92be8ae66d9f4a457af00c1
swang520 <swang520@student.ubc.ca>
2024-11-10 15:08:29 -0800

    update to app

--- app.js
+++ app.js
@@ -5,10 +5,9 @@ const Users = require('./Users');
 const User = require('./User');
 
 let users = new Users();
+let user;
 
 app.use(express.static(__dirname));
-
-
 
 app.get('/newUser', (req, res) => {
   res.sendFile(path.join(__dirname, 'newUser.html'));
@@ -18,7 +17,7 @@ app.use(express.json());
 
 app.post('/submit', (req, res) => {
   const userData = req.body;
-  let user = new User(userData.userName, "", userData.gender, userData.preferToRoomWith, userData.sleepLevel, userData.cleanLevel, userData.noiseLevel, userData.partyLevel);
+  user = new User(userData.userName, "", userData.gender, userData.preferToRoomWith, userData.sleepLevel, userData.cleanLevel, userData.noiseLevel, userData.partyLevel);
   users.addUser(user);
   console.log(users.users);
   res.json({ redirect: '/mainMenu' });
@@ -26,10 +25,13 @@ app.post('/submit', (req, res) => {
 
 app.get('/mainMenu', (req, res) => {
   res.sendFile(path.join(__dirname, 'mainMenu.html'));
+  matches = users.findBestMatches(user);
 });
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
 });
+
+
 
 
