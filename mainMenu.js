7939e0bdaedee47569dd63483b88b14fd89e72cd
swang520 <swang520@student.ubc.ca>
2024-11-10 15:43:34 -0800

    fixing tab thing

--- mainMenu.js
+++ mainMenu.js
@@ -6,29 +6,11 @@ function openTab(tabId) {
 }
 
 function loadChatContent() {
-    // Show the chat tab
-    openTab('chat');
-    
-    // Fetch and load content from chatBox.html
-    fetch('chatBox.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('chat').innerHTML = html;
-        })
-        .catch(error => console.error('Error loading chat content:', error));
+    window.location.href = 'http://127.0.0.1:5500/chatBox.html';
 }
 
 function loadprofileContent() {
-    // Show the chat tab
-    openTab('myprofile');
-    
-    // Fetch and load content from chatBox.html
-    fetch('newUser.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('myprofile').innerHTML = html;
-        })
-        .catch(error => console.error('Error loading chat content:', error));
+    window.location.href = 'http://127.0.0.1:5500/newUser.html';
 }
 
 function loadsignoutContent() {
@@ -36,7 +18,7 @@ function loadsignoutContent() {
 }
 
 function nextSkipUser() {
-    
+
 }
 
 function nextKeepUser() {
