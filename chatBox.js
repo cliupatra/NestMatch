2c6d33b30f7f8e2fd39042d95c50f3abeef944d5
hwang168 <hwang168@students.cs.ubc.ca>
2024-11-10 17:42:45 -0800

    Updated chat box

--- chatBox.js
+++ chatBox.js
@@ -18,6 +18,21 @@ const messagesData = {
         {type:'recieved', text: 'One question...'},
         {type:'recieved', text: 'Whyy so serious???'},
         {type:'recieved', text: 'Those who know *skull emoji*'},
+    ], 
+
+    Levi: [
+        {type:'recieved', text: 'Hi Im a very neat person, would you like to rent with me?'},
+        {type:'sent', text: 'Sorry I already have a roommate now!'}
+    ], 
+
+    Jane: [
+        {type:'recieved', text: 'Hi do you wanna rent, we matched very well!'},
+        {type:'sent', text: 'Yeah sure, what year are you?'},
+        {type:'recieved', text: 'Im second year in cs!'},
+        {type:'sent', text: 'Oh wow me too!'},
+        {type:'sent', text: 'Do you wanna meet in the nest tomrrow at 6?'},
+        {type:'recieved', text: 'yeah sure!'},
+
     ]
 
 
